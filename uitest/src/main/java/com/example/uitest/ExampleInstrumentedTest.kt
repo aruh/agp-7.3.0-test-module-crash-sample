bfd3e7a931ee2b117b0891ce96ca05271bd782a2
Alexander Ruhland <alexander.b.ruhland@gmail.com>
2022-10-04 09:39:49 +0200

    Initial commit

---
+++ uitest/src/main/java/com/example/uitest/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package com.example.uitest
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
+import com.example.androidtestmoduletest.MainActivity
+import com.example.androidtestmoduletest.R
+
+import org.junit.Test
+import org.junit.Rule
+
+class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun showsHelloWorld() {
+        onView(withId(R.id.hello_world)).check(matches(withText("Hello World!")))
+    }
+}
